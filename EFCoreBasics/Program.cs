5735e737b3246cdec4e25c8e8869fe603211a837
Victor Luongo <victor.luongo@gmail.com>
2022-02-10 22:52:28 -0300

    Initial project

---
+++ EFCoreBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
